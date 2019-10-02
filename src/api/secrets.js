e29892a5397b2f059ec8667ce7ff73dcad7cf285
Danzel Rana <danzelrana@gmail.com>
2019-10-02 11:52:07 -0700

    api calls

---
+++ src/api/secrets.js
@@ -0,0 +1,3 @@
+const ClientId = "oj5sely04mm226nxlcg9uu7maodn2n";
+
+export default ClientId;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import StreamerList from "../components/StreamerList";
 import { Jumbotron, Container, Button } from "reactstrap";
 import "./App.css";
+import clientId from "../api/secrets";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
@@ -10,12 +12,13 @@ class App extends Component {
   }
 
   async onButtonPress() {
+    const oauth = `Bearer ${document.location.hash.substring(14, 44)}`;
+    console.log(oauth);
+    axios.defaults.headers.common["Authorization"] = oauth;
+
     try {
-      const response = await fetch(
-        "https://id.twitch.tv/oauth2/authorize?client_id=oj5sely04mm226nxlcg9uu7maodn2n&redirect_uri=http://localhost&response_type=token&scope=channel_feed_read"
-      );
-      const data = await response.json();
-      console.log(data);
+      const response = await axios.get(`https://api.twitch.tv/helix/users`);
+      console.log(response);
     } catch (error) {
       console.log("ERROR BRO: ", error);
     }
@@ -26,11 +29,12 @@ class App extends Component {
       <div className="d-flex align-items-center min-vh-100">
         <Container className="text-center">
           <a
-            href="https://id.twitch.tv/oauth2/authorize?client_id=oj5sely04mm226nxlcg9uu7maodn2n&redirect_uri=http://localhost:3000/&response_type=token&scope=channel_feed_read"
+            href={`https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=http://localhost:3000/&response_type=token&scope=channel_feed_read`}
             className="btn btn-primary"
           >
             Sign In
           </a>
+          <Button onClick={() => this.onButtonPress()}>CLICK ME!</Button>
         </Container>
       </div>
     );
