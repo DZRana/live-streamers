701a1c340bc762da81975705ad0f2b5127e6e40f
Danzel Rana <danzelrana@gmail.com>
2019-11-30 20:29:29 -0800

    Updates

---
+++ precache-manifest.7db7e1f49f6094dc9a736ee571fa7d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3f4c0a41d690ebab36e6cc867e5292",
+    "url": "/twitch-live/index.html"
+  },
+  {
+    "revision": "5cbef0bad835f7dfe2b3",
+    "url": "/twitch-live/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7847e5bc8da2cadf132a",
+    "url": "/twitch-live/static/css/main.b882da27.chunk.css"
+  },
+  {
+    "revision": "5cbef0bad835f7dfe2b3",
+    "url": "/twitch-live/static/js/2.cdce5904.chunk.js"
+  },
+  {
+    "revision": "7847e5bc8da2cadf132a",
+    "url": "/twitch-live/static/js/main.6560f51d.chunk.js"
+  },
+  {
+    "revision": "ac17f871e139f0cc3719",
+    "url": "/twitch-live/static/js/runtime-main.f53b1bc3.js"
+  }
+]);
